4e2626f945e48d32754dd615600d7f5612de8704
dailey.yet <dailey.yet@gmail.com>
2015-07-27 16:02:55 +0800

    completed gh-pages index.html

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,12 @@
-console.log('This would be the main JS file.');
+(function(win,$){
+	openCollapse = function(){
+		$(".collapse").click(function(){
+			var $trigger = $(this);
+//			$trigger.attr("title","Expand/Collapse");
+			var target_id = $trigger.data("target");
+			if(target_id==undefined || target_id ==null){return;}
+			$(target_id).toggleClass("hiden");
+		});
+	}
+	openCollapse();
+})(window,jQuery);
